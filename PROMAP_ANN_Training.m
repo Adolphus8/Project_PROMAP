96e4fe8a115bbb149ce89dc1c7983fc835a9df12
Adolphus Lye <46358970+Adolphus8@users.noreply.github.com>
2022-04-19 00:48:31 +0100

    Add files via upload

--- PROMAP_ANN_Training.m
+++ PROMAP_ANN_Training.m
@@ -45,18 +45,6 @@ time1(1) = toc;
 y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
 save('ANN7_CreepRupture_Frac', 'net', 'tr', 'time1', 'r2_score')
 
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,1), 4); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
-save('ANN1_Prinja_CreepRupture_Fracture', 'net', 'tr', 'time1', 'r2_score')
-
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,1), 7); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
-save('ANN2_Prinja_CreepRupture_Fracture', 'net', 'tr', 'time1', 'r2_score')
-
 fprintf('Fit Creep Rupture Elongation \n')
 tic;
 [net,tr] = fit_NN(input_feature, target_feature(:,2), 1); 
@@ -87,18 +75,6 @@ tic;
 time1(1) = toc;
 y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
 save('ANN5_CreepRupture_Elong', 'net', 'tr', 'time1', 'r2_score')
-
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,2), 4); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
-save('ANN1_Prinja_CreepRupture_Elong', 'net', 'tr', 'time1', 'r2_score')
-
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,2), 5); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
-save('ANN2_Prinja_CreepRupture_Elong', 'net', 'tr', 'time1', 'r2_score')
 
 fprintf('Fit Creep Rupture RA \n')
 tic;
@@ -131,6 +107,42 @@ time1(1) = toc;
 y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,3), y_pred);
 save('ANN5_CreepRupture_RA', 'net', 'tr', 'time1', 'r2_score')
 
+%% Train Existing ANN by N. Prinja for Creep Rupture Properties prediction:
+% To be trained with experimental data:
+clear; clc;
+
+table1a = readtable('Creep_rupture_data_processed.csv'); 
+creep_mat = table2array(table1a);                        % Convert tabular data into array
+tra = find(creep_mat(:,1) == 13); creep_mat(tra,:) = []; % Remove data whose composition is 0 for all elements
+input_feature_real = creep_mat(:,[1:4,8:26]); target_feature_real = creep_mat(:,[5:7]);
+
+% Model to compute R2-score in [%]:
+score_model = @(data, model_output) (1 - (sum((data - model_output).^2)./sum((data - mean(data)).^2)))*100;
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,1), 4); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
+save('ANN1_Prinja_CreepRupture_Fracture', 'net', 'tr', 'time1', 'r2_score')
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,1), 7); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
+save('ANN2_Prinja_CreepRupture_Fracture', 'net', 'tr', 'time1', 'r2_score')
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,2), 4); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
+save('ANN1_Prinja_CreepRupture_Elong', 'net', 'tr', 'time1', 'r2_score')
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,2), 5); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
+save('ANN2_Prinja_CreepRupture_Elong', 'net', 'tr', 'time1', 'r2_score')
+
 tic;
 [net,tr] = fit_NN(input_feature_real, target_feature_real(:,3), 4); 
 time1(1) = toc;
@@ -144,6 +156,7 @@ y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feat
 save('ANN2_Prinja_CreepRupture_RA', 'net', 'tr', 'time1', 'r2_score')
 
 %% Train the ANN for Tensile Properties prediction
+clear; clc;
 
 table1a = readtable('Tensile_syndata_small.csv'); 
 syndata_mat = table2array(table1a);
@@ -189,18 +202,6 @@ time1(1) = toc;
 y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
 save('ANN6_Tensile_PS02', 'net', 'tr', 'time1', 'r2_score')
 
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,1), 4); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
-save('ANN1_Prinja_Tensile_PS02', 'net', 'tr', 'time1', 'r2_score')
-
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,1), 6); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
-save('ANN2_Prinja_Tensile_PS02', 'net', 'tr', 'time1', 'r2_score')
-
 fprintf('Fit Tensile UTS \n')
 tic;
 [net,tr] = fit_NN(input_feature, target_feature(:,2), 1); 
@@ -231,18 +232,6 @@ tic;
 time1(1) = toc;
 y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
 save('ANN6_Tensile_UTS', 'net', 'tr', 'time1', 'r2_score')
-
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,2), 4); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
-save('ANN1_Prinja_Tensile_UTS', 'net', 'tr', 'time1', 'r2_score')
-
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,2), 6); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
-save('ANN2_Prinja_Tensile_UTS', 'net', 'tr', 'time1', 'r2_score')
 
 fprintf('Fit Tensile Elongation \n')
 tic;
@@ -275,18 +264,6 @@ time1(1) = toc;
 y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,3), y_pred);
 save('ANN5_Tensile_Elong', 'net', 'tr', 'time1', 'r2_score')
 
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,3), 4); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,3), y_pred);
-save('ANN1_Prinja_Tensile_Elongation', 'net', 'tr', 'time1', 'r2_score')
-
-tic;
-[net,tr] = fit_NN(input_feature_real, target_feature_real(:,3), 5); 
-time1(1) = toc;
-y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,3), y_pred);
-save('ANN2_Prinja_Tensile_Elongation', 'net', 'tr', 'time1', 'r2_score')
-
 fprintf('Fit Tensile RofA \n')
 tic;
 [net,tr] = fit_NN(input_feature, target_feature(:,4), 1); 
@@ -317,6 +294,54 @@ tic;
 time1(1) = toc;
 y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,4), y_pred);
 save('ANN6_Tensile_RofA', 'net', 'tr', 'time1', 'r2_score')
+
+%% Train Existing ANN by N. Prinja for Tensile Properties prediction:
+% To be trained with experimental data:
+clear; clc;
+
+table1a = readtable('Tensile_data_processed.csv'); 
+tensile_mat = table2array(table1a);                          % Convert tabular data into array
+tra = find(tensile_mat(:,1) == 13); tensile_mat(tra,:) = []; % Remove data whose composition is 0 for all elements
+input_feature_real = tensile_mat(:,[1:3,8:26]); target_feature_real = tensile_mat(:,[4:7]);
+
+% Model to compute R2-score in [%]:
+score_model = @(data, model_output) (1 - (sum((data - model_output).^2)./sum((data - mean(data)).^2)))*100;
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,1), 4); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
+save('ANN1_Prinja_Tensile_PS02', 'net', 'tr', 'time1', 'r2_score')
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,1), 6); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,1), y_pred);
+save('ANN2_Prinja_Tensile_PS02', 'net', 'tr', 'time1', 'r2_score')
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,2), 4); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
+save('ANN1_Prinja_Tensile_UTS', 'net', 'tr', 'time1', 'r2_score')
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,2), 6); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,2), y_pred);
+save('ANN2_Prinja_Tensile_UTS', 'net', 'tr', 'time1', 'r2_score')
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,3), 4); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,3), y_pred);
+save('ANN1_Prinja_Tensile_Elongation', 'net', 'tr', 'time1', 'r2_score')
+
+tic;
+[net,tr] = fit_NN(input_feature_real, target_feature_real(:,3), 5); 
+time1(1) = toc;
+y_pred = transpose(net(input_feature_real')); r2_score = score_model(target_feature_real(:,3), y_pred);
+save('ANN2_Prinja_Tensile_Elongation', 'net', 'tr', 'time1', 'r2_score')
 
 tic;
 [net,tr] = fit_NN(input_feature_real, target_feature_real(:,4), 4); 
